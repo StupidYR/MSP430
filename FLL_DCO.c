3a670bded82e51b9fb2696e7a6771c17589e30b4
Yourui232 <52345637+Yourui232@users.noreply.github.com>
2020-01-26 15:08:26 +0800

    MSP430F5529LP
    通过FLL使DCO运行在2.45MHz

--- FLL_DCO.c
+++ FLL_DCO.c
@@ -9,45 +9,45 @@ void main(void)
         P7DIR |= BIT7;               // MCLK 
 	P7SEL |= BIT7;
 
-	UCSCTL6 &= ~(XT1OFF);         // ʹ�� XT1
-	UCSCTL6 |= XCAP_3;            // ���ø��ص���
+	UCSCTL6 &= ~(XT1OFF);         // 使能 XT1
+	UCSCTL6 |= XCAP_3;            // 配置负载电容
         
-        UCSCTL3 = SELREF_2;           // ѡ��REFO����ΪFLL�Ĳο�ʱ��Դ
+        UCSCTL3 = SELREF_2;           // 选择REFO来作为FLL的参考时钟源
         
-// ���ʱ��Դ�Ƿ��ȶ�
+// 检测时钟源是否稳定
     do
     {
-    	UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG); // ��� XT2,XT1,DCO �� fault flags
+    	UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG); // 清除 XT2,XT1,DCO 的 fault flags
                                  
-    	SFRIFG1 &= ~OFIFG;        // ������ϱ�־
-    }while (SFRIFG1 & OFIFG);       // ����������ϱ�־
+    	SFRIFG1 &= ~OFIFG;        // 清除故障标志
+    }while (SFRIFG1 & OFIFG);       // 监测振荡器故障标志
 
-// ����DCO��Ƶ��Ϊ 2.45MHz
+// 配置DCO的频率为 2.45MHz
     
-    __bis_SR_register(SCG0);      // �ر�FLL���ƣ�   ������رյĻ���FLL�Ĵ�������ı�
-    UCSCTL0 = 0x0000;             // ���� DCOx = 0 , MODx = 0  ��
-    UCSCTL1 = DCORSEL_3;          // ����DCO��Ƶ�ʷ�Χ 1.51��������6.07 
+    __bis_SR_register(SCG0);      // 关闭FLL控制；   如果不关闭的话，FLL寄存器不会改变
+    UCSCTL0 = 0x0000;             // 配置 DCOx = 0 , MODx = 0  ；
+    UCSCTL1 = DCORSEL_3;          // 配置DCO的频率范围 1.51————6.07 
     
-    //  f_DCO(n, 0)_MAX �� f_DCO �� f_DCO(n, 31)_MIN
-    // ��Χ�μ������ֲ� DCO Frequency
+    //  f_DCO(n, 0)_MAX ≤ f_DCO ≤ f_DCO(n, 31)_MIN
+    // 范围参见数据手册 DCO Frequency
     
-    UCSCTL2 = FLLD_1 + 74;        // ����FLL_D������Ƶ    ����FLL_N��74
+    UCSCTL2 = FLLD_1 + 74;        // 配置FLL_D：二分频    配置FLL_N：74
     
     // (N + 1) * FLLRef = Fdco
     // (74 + 1) * 32768 = 2.45MHz
     
-    __bic_SR_register(SCG0);      // ����FLL����
+    __bic_SR_register(SCG0);      // 开启FLL控制
     
     // 32 x 32 x Fdco / f_FLL_reference = waitting_time
     // 32 x 32 x 2.45 MHz / 32.768 Hz = 76563 ---->  MCLK cycles for DCO to settle
     __delay_cycles(76563);
     
-// ���ʱ��Դ�Ƿ��ȶ�
+// 检测时钟源是否稳定
     do
     {
-        UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + 0x0004 + DCOFFG); // ��� XT2,XT1,DCO �� fault flags
-        SFRIFG1 &= ~OFIFG;         // ������ϱ�־
-    }while (SFRIFG1 & OFIFG);        // ����������ϱ�־
+        UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + 0x0004 + DCOFFG); // 清除 XT2,XT1,DCO 的 fault flags
+        SFRIFG1 &= ~OFIFG;         // 清除故障标志
+    }while (SFRIFG1 & OFIFG);        // 监测振荡器故障标志
 
     while(1);                      // Loop in place
 }
