33e05e03caa3845e4d95e78a3c6f27e6773eca99
Yourui232 <52345637+Yourui232@users.noreply.github.com>
2020-01-29 14:43:24 +0800

    利用Timer_A使P1.2输出75%，P1.3输出25%的PWM波形

--- Timer_PWM.c
+++ Timer_PWM.c
@@ -3,16 +3,22 @@ void main(void)
 {
       WDTCTL = WDTPW + WDTHOLD;         
       P1DIR |= BIT2+BIT3;               
-      P1SEL |= BIT2+BIT3;               // P1^2��P1^3���Ź���ѡΪ��ʱ�����
+      P1SEL |= BIT2+BIT3;               // P1^2和P1^3引脚功能选为定时器输出
       
-      TA0CCR0 = 512-1;                  // PWM���ڶ���
-      TA0CCTL1 = OUTMOD_7;              // CCR1�Ƚ����ģʽ7����λ/��λ
-      TA0CCR1 = 384;                    // CCR1 PWM ռ�ձȶ���
-      // ��ģʽ��ռ�ձ� = TA0CCR1 / TA0CCR0          ��������>3/4
+      TA0CCR0 = 512-1;                  // PWM周期定义；周期时长从 0 到 TA0CCR0 共计 TA0CCR0 + 1
+      
+      TA0CCTL1 = OUTMOD_7;              // CCR1比较输出模式7：复位/置位
+      TA0CCR1 = 384;                    // CCR1 PWM 占空比定义
+      // 此模式下占空比 = TA0CCR1 / TA0CCR0 
+      //占空比75%
+      
+      TA0CCTL1 = OUTMOD_7;
+      TA0CCR1 = 128;
+      //占空比25%
       
       TA0CTL = TASSEL_1 + ID_0 + MC_1 + TACLR;	
-      // ����Timer_A�Ŀ��ƼĴ���TA_CTL
-      //TASSEL��ѡ��ACLKʱ��Դ��ID��ѡ���Ƶ��MC��ѡ��������ģʽ��TACLR�����TAR������
+      // 配置Timer_A的控制寄存器TA_CTL
+      //TASSEL：选择ACLK时钟源；ID：选择分频；MC：选择增计数模式；TACLR：清除TAR计数器
       
-      __bis_SR_register(LPM3_bits);    	// ����LPM3����ģʽ
+      __bis_SR_register(LPM3_bits);    	// 进入LPM3功耗模式
 }
