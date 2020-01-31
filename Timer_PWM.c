b2ba3ba5fa8e894951e475133c748ba0ffbfb7b1
Yourui232 <52345637+Yourui232@users.noreply.github.com>
2020-01-31 15:20:25 +0800

    Timer_A0比较模式使P1.2和P1.3分别输出75%，25%的PWM波

--- Timer_PWM.c
+++ Timer_PWM.c
@@ -1,24 +1,49 @@
+/*********************Timer_A0****************************
+
+P1.2引脚：输出Timer_A0_CCR1方波
+P1.3引脚：输出Timer_A0_CCR2方波
+
+TA0CTL：Timer_A0的控制寄存器
+
+TA0CCTLn：Timer_A0的捕获 /比较控制 n寄存器（默认为比较模式）
+
+TA0CCRn：Timer_A0的捕获 /比较 n寄存器        
+
+************************END****************************/
+
 #include <msp430f5529.h>
+
+void clock_check();
+
 void main(void)
 {
-      WDTCTL = WDTPW + WDTHOLD;         
+      WDTCTL = WDTPW + WDTHOLD;
       P1DIR |= BIT2+BIT3;               
       P1SEL |= BIT2+BIT3;               // P1^2和P1^3引脚功能选为定时器输出
       
-      TA0CCR0 = 512-1;                  // PWM周期定义；周期时长从 0 到 TA0CCR0 共计 TA0CCR0 + 1
+      clock_check();
       
+      TA0CCR0 = 512-1;                  // PWM周期定义
       TA0CCTL1 = OUTMOD_7;              // CCR1比较输出模式7：复位/置位
       TA0CCR1 = 384;                    // CCR1 PWM 占空比定义
-      // 此模式下占空比 = TA0CCR1 / TA0CCR0 
-      //占空比75%
+      // 此模式下占空比 = TA0CCR1 / TA0CCR0  
+      ////占空比为 75%
       
-      TA0CCTL1 = OUTMOD_7;
-      TA0CCR1 = 128;
-      //占空比25%
+      TA0CCTL2 = OUTMOD_7;              // CCR2 比较输出模式7：复位/置位
+      TA0CCR2 = 128;                    // CCR2 PWM 占空比定义
+      //占空比为 25%
       
-      TA0CTL = TASSEL_1 + ID_0 + MC_1 + TACLR;	
-      // 配置Timer_A的控制寄存器TA_CTL
-      //TASSEL：选择ACLK时钟源；ID：选择分频；MC：选择增计数模式；TACLR：清除TAR计数器
+      TA0CTL = TASSEL_1 + MC_1 + TACLR;	
+      //TASSEL：选择ACLK时钟源；MC：选择增计数模式；TACLR：清除TAR计数器
       
       __bis_SR_register(LPM3_bits);    	// 进入LPM3功耗模式
+}
+void clock_check()
+{    
+      do 
+        {
+          UCSCTL7 &= ~(XT2OFFG + XT1LFOFFG + DCOFFG);
+          SFRIFG1 &= ~OFIFG; 
+        }
+      while (SFRIFG1 & OFIFG);           
 }
