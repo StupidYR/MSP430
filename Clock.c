c7c94549ffb1545320df3bacc730e6d2f68bb0f1
Yourui232 <52345637+Yourui232@users.noreply.github.com>
2020-01-31 09:09:15 +0800

    低频状态下配置SMCLK，ACLK

--- Clock.c
+++ Clock.c
@@ -1,3 +1,23 @@
+/*****************Clock********************
+            上电时的默认状态
+
+MCLK：主系统时钟；DCOCLKDIV
+
+SMCLK：子系统时钟；DCOCLKDIV
+
+ACLK：辅助系统时钟；XT1（若XT1无效，则为REFO；其他情况切换为DCO）
+
+系统稳定后，DCOCLK默认为 2.097152MHZ，
+FLL默认2分频，则MCLK和SMCLK的频率都为1.048576MHZ。
+
+此外FLL默认参照源为XT1
+
+如果连接XT1和XT2的引脚不进行PxSEL的设置，那么这两个时钟源都是无效的；
+
+REFOCLK、VLOCLK、DCOCLK默认状态下是可用的；
+
+
+*******************END**********************/
 #include <msp430f5529.h>
 
 void clock_check();
@@ -14,7 +34,7 @@ void main(void)
     P7SEL |= BIT7;
 
     P5SEL |= BIT2+BIT3;          // 启用第二功能 XT2 IN/OUT
-    UCSCTL6 &= ~(XT1OFF + XT2OFF);  // 配置UCSTRL 使能XT1 & XT2 
+    UCSCTL6 &= ~(XT1OFF + XT2OFF);  // 使能XT1 & XT2 
     UCSCTL6 |= XCAP_3;           // 配置负载电容
         
     clock_check();
